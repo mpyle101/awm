ded005957aac394d9fc3b9498e41c5772a8e1cbf
Michael Pyle <mpyle101@gmail.com>
2018-08-01 09:30:04 -0600

    Break cycles out into their own collection

--- workouts/import.sh
+++ workouts/import.sh
@@ -1,5 +1,6 @@
 #! /usr/local/bin/bash
 
 mongo awm --eval "db.dropDatabase()"
-mongoimport --db awm --collection legend --file legend.json --jsonArray
+mongoimport --db awm --collection cycles   --file cycles.json   --jsonArray
+mongoimport --db awm --collection legend   --file legend.json   --jsonArray
 mongoimport --db awm --collection workouts --file workouts.json --jsonArray
