fed5194821733043c0731c28747b5bdcdf960453
Michael Pyle <mpyle101@gmail.com>
2018-08-06 22:40:23 -0600

    Add BR block support

--- client/src/app/blocks/training-block.component.ts
+++ client/src/app/blocks/training-block.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
     selector: 'awm-tr-block',
-    templateUrl: './training-block.component.html'
+    templateUrl: './training-block.component.html',
+    styleUrls: ['./training-block.component.scss']
 })
 export class TrainingBlockComponent implements OnInit {
 
