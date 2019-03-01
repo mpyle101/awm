5400d7133c496c38ba1367b2c092b0af87f9d6e3
Michael Pyle <mpyle101@gmail.com>
2019-03-01 08:59:34 -0700

    Max strength pupup for table widget

--- client-rx/src/components/Block.js
+++ client-rx/src/components/Block.js
@@ -3,6 +3,7 @@ import 'moment-duration-format'
 
 import React from 'react'
 import withStyles from 'react-jss'
+import { Popup } from 'semantic-ui-react'
 
 const styles = {
     cell: {
@@ -16,6 +17,12 @@ const styles = {
 const duration = period => {
     const d = moment.duration(period)
     return d.format(() => d.asMinutes() > 59 ? 'h:mm' : 'm[m]')
+}
+
+const formatReps = reps => Array.isArray(reps) ? `[${reps.join(",")}]` : reps
+const formatSet  = (item, set) => {
+    const reps = formatReps(set.reps)
+    return `${item.key}: ${set.count}x${reps}@${set.wt}`
 }
 
 const Block = props => {
@@ -41,6 +48,21 @@ const Block = props => {
                     <div className={classes.item}>{duration(work)}</div>
                     <div>{meta}</div>
                 </div>
+            )
+
+        case 'MS':
+            return (
+                <Popup trigger={<div>MS</div>}>
+                    {work.map(item => (
+                        <div key={item.key}>
+                            {item.sets.map((set, idx) => (
+                                <div key={item.key + idx}>
+                                    {formatSet(item, set)}
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </Popup>
             )
 
         default:
