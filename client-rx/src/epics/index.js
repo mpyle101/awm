9ffadb61b161336ec63c3eaf39c39d73366b497c
Michael Pyle <mpyle101@gmail.com>
2019-03-30 17:11:53 -0600

    Add canceling of workouts fetch

--- client-rx/src/epics/index.js
+++ client-rx/src/epics/index.js
@@ -1,13 +1,23 @@
 import * as moment from 'moment'
 import { combineEpics, ofType } from 'redux-observable'
 import { of as observableOf } from 'rxjs'
-import { catchError, debounceTime, map, switchMap } from 'rxjs/operators'
+import {
+    catchError,
+    debounceTime,
+    map,
+    switchMap,
+    takeUntil
+} from 'rxjs/operators'
 
 import {
     fetchWorkoutsSuccess,
     fetchWorkoutsFailure
 } from '../actions'
-import { SELECT_DATE, SELECT_PERIOD } from '../actions/types'
+import {
+    SELECT_DATE,
+    SELECT_PERIOD,
+    FETCH_WORKOUTS_CANCEL
+} from '../actions/types'
 
 import { fetchWorkouts } from '../apis'
 
@@ -19,6 +29,7 @@ const fetchWorkoutsEpic = action$ =>
         ofType(SELECT_DATE, SELECT_PERIOD),
         debounceTime(200),
         switchMap(({payload: {date, period}}) => fetchWorkouts(date, period), (a, resp) => resp),
+        takeUntil(action$.ofType(FETCH_WORKOUTS_CANCEL)),
         map(workouts => process(workouts)),
         map(workouts => fetchWorkoutsSuccess(workouts)),
         catchError(err => observableOf(fetchWorkoutsFailure(err.message)))
